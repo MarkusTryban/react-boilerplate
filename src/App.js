c885ecbbaa7339e02e5b3445d9c47c8fb78f4a4d
MarkusTryban <markustryban@gmail.com>
2019-09-04 21:25:16 -0400

    remove state, increment & decrement methods

--- src/App.js
+++ src/App.js
@@ -2,18 +2,6 @@ import React from 'react'
 import { hot } from 'react-hot-loader'
 
 class App extends React.Component {
-  state = {
-    count: 0
-  }
-
-  increment = () => {
-    this.setState(state => ({ count: state.count + 1 }))
-  }
-
-  decrement = () => {
-    this.setState(state => ({ count: state.count - 1 }))
-  }
-
   render() {
     const { count } = this.state
     return (
